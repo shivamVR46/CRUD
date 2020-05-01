0af33a4f92e9f031eb62d8803a7a1702a417371c
shivamVR46 <shivamcharusat@gmail.com>
2020-04-30 21:09:52 -0400

    Connection to MongoDB Cloud Database

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 console.log('Server')
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
 
 
-app.listen(8081,function(){
-    console.log('listening on 8081')
-})
+MongoClient.connect('mongodb+srv://admin:admin@cluster0-h77dy.mongodb.net/test?retryWrites=true&w=majority',
+    {
+        useUnifiedTopology: true
+    }, function (err, client) {
+
+        const db = client.db('CRUD')
+        const quotesCollection = db.collection('quotes')
+
+        if (err) return console.error(err)
+
+        app.use(bodyParser.urlencoded({ extended: true }))
+
+        app.listen(8081, function () {
+            console.log('listening on 8081')
+        })
+
+
+        app.get('/', function (request, response) {
+            response.sendFile(/CRUD/ + '/index.html')
+        })
+
+        app.post('/quotes', function (request, response) {
+            quotesCollection.insertOne(request.body)
+            .then(result =>{
+                response.redirect('/')
+            }).catch(err => console.error(err))
+        })
+    })
+
+
